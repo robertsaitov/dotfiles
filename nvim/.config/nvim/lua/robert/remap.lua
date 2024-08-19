b9485cb979b253e7ce29d30efffe270220133844
Robert Saitov <38866900+robertsaitov@users.noreply.github.com>
2024-08-19 16:44:52 -0500

    add quickfix navigation

--- nvim/.config/nvim/lua/robert/remap.lua
+++ nvim/.config/nvim/lua/robert/remap.lua
@@ -25,6 +25,10 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.local/scripts/tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+-- Quickfix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
 -- Replace current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
