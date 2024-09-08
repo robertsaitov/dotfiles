b4db4ff180792621bf251da90d9b52c1c384815e
robertsaitov <robert.saitov30@gmail.com>
2024-09-07 22:17:43 -0500

    update plugins

--- nvim/.config/nvim/lua/robert/lazy/notify.lua
+++ nvim/.config/nvim/lua/robert/lazy/notify.lua
@@ -1,7 +1,10 @@
 return {
-    'rcarriga/nvim-notify',
+    "rcarriga/nvim-notify",
     opts = {
         background_colour = "#000000",
     },
     config = true,
+    vim.keymap.set("n", "<Esc>", function()
+        require("notify").dismiss()
+    end, { desc = "dismiss notify popup and clear hlsearch" }),
 }
