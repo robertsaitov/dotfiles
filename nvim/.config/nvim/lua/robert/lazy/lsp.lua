75f502f0c503b6290997bd8b089b3cd00fa8a991
robertsaitov <robert.saitov30@gmail.com>
2024-12-08 19:16:23 -0600

    add formatter and linter, remove smartfolding

--- nvim/.config/nvim/lua/robert/lazy/lsp.lua
+++ nvim/.config/nvim/lua/robert/lazy/lsp.lua
@@ -47,13 +47,6 @@ return {
             },
         })
 
-        -- for conciseness
-        local formatting = null_ls.builtins.formatting -- to setup formatters
-        local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-
-        -- to setup format on save
-        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
         -- configure null_ls
         null_ls.setup({
             -- add package.json as identifier for root (for typescript monorepos)
@@ -64,57 +57,6 @@ return {
                 ".git",
                 "package.json"
             ),
-            -- setup formatters & linters
-            sources = {
-                --  to disable file types use
-                --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-                formatting.stylua, -- lua formatter
-                formatting.isort,
-                formatting.black, -- python formatter
-                diagnostics.mypy,
-            },
-
-            -- configure format on save
-            --[[ on_attach = function(current_client, bufnr)
-				if current_client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({
-								filter = function(client)
-									--  only use null-ls for formatting instead of lsp server
-									return client.name == "null-ls"
-								end,
-								async = true,
-								bufnr = bufnr,
-							})
-						end,
-					})
-				end
-			end, ]]
-        })
-
-        local prettier = require("prettier")
-
-        prettier.setup({
-            bin = "prettierd",
-            filetypes = {
-                "css",
-                "graphql",
-                "html",
-                "javascript",
-                "javascriptreact",
-                "json",
-                "less",
-                "markdown",
-                "scss",
-                "typescript",
-                "typescriptreact",
-                "yaml",
-                "eslint",
-            },
         })
 
         require("fidget").setup({})
--- nvim/.config/nvim/lua/robert/remap.lua
+++ nvim/.config/nvim/lua/robert/remap.lua
@@ -23,7 +23,6 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.local/scripts/tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- Quickfix navigation
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
